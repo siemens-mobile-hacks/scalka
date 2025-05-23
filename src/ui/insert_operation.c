7811644ae5604f19b12ca9d1326850cc189c0365
Philip Minakov <sir.cadogan@protonmail.com>
2025-05-23 15:26:58 +0400

    Redesign insert operation ui

--- src/ui/insert_operation.c
+++ src/ui/insert_operation.c
@@ -1,143 +1,112 @@
 #include <swilib.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../settings.h"
 #include "../operation.h"
 #include "keydesc.h"
 #include "insert_operation.h"
 
 typedef struct {
-    GUI gui;
-    UI_DATA *ui_data;
-    WSHDR *ws;
     int state;
-} MAIN_GUI;
-
-static RECT CANVAS = {0, 0, 0, 0};
+    WSHDR *ws;
+    UI_DATA *ui_data;
+} INSERT_OPERATION_DATA;
 
 extern CALC_SETTINGS SETTINGS;
 
-static inline int getXXXXwidth(int font) {
-    return (GetSymbolWidth('X', font) * 4);
-}
+static void OnRedraw(GUI *) {
+    GUI *gui = GetTopGUI();
+    INSERT_OPERATION_DATA *data = GetPopupUserPointer(gui);
 
-static void OnRedraw(MAIN_GUI* data) {
-    int scr_w = ScreenW();
-    int scr_h = ScreenH();
-    int need_height;
-    int need_width;
-    int start_y;
-    int start_x;
-    int XXXXwidth = getXXXXwidth(FONT_SMALL);
-    int Ysize = GetFontYSIZE(FONT_SMALL);
+    int x = gui->canvas->x;
+    int y = gui->canvas->y;
+    int x2 = gui->canvas->x2;
+    int y2 = gui->canvas->y2;
 
-    need_height = Ysize * 4 + 5 * 2 * 3 + 5 * 2;
-    need_width = XXXXwidth * 3 + 5 * 2 * 2 + 5 * 2;
+    int w = (x2 - x - 20) / 3;
+    int h = (y2 - y - 20) / 4;
 
-    start_y = need_height < scr_h ? (scr_h - need_height) >> 1 : 0;
-    start_x = need_width < scr_w ? (scr_w - need_width) >> 1 : 0;
+    x += 10;
+    y += 10;
+    const int font = FONT_SMALL;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            int x_item = x + w * i;
+            int y_item = y + h * j;
+            int x2_item = x_item + w;
+            int y2_item = y_item + h;
+            DrawRectangle(x_item, y_item, x2_item, y2_item, 0,
+                GetPaletteAdrByColorIndex(PC_FOREGROUND), GetPaletteAdrByColorIndex(0x17));
+            wsprintf(data->ws, "%t", KEY_DESC[data->state + 1][j * 3 + i]);
 
-    DrawRoundedFrame(start_x, start_y, start_x + need_width, start_y + need_height, 3, 3, 0,
-                     GetPaletteAdrByColorIndex(0), GetPaletteAdrByColorIndex(20));
-
-    for (int y = 0; y < 4; y++) {
-        for (int x = 0; x < 3; x++) {
-            unsigned int x_frame = start_x + XXXXwidth * x + 5 * 2 * x;
-            unsigned int y_frame = start_y + Ysize * y + 5 * 2 * y;
-
-
-            wsprintf(data->ws, "%t", KEY_DESC[data->state][y*3+x]);
-
-            unsigned int str_width = Get_WS_width(data->ws, FONT_SMALL);
-            unsigned int x_str = x_frame + ((XXXXwidth - str_width) >> 1) + 5;
-
-            DrawRoundedFrame(x_frame, y_frame, x_frame + XXXXwidth + 5 * 2, y_frame + Ysize + 5 * 2, 3, 3, 0,
-                             GetPaletteAdrByColorIndex(0), GetPaletteAdrByColorIndex(20));
-            DrawString(data->ws, x_str, y_frame + 5, x_str + str_width, y_frame + Ysize + 5,
-                       FONT_SMALL, 0, GetPaletteAdrByColorIndex(0), GetPaletteAdrByColorIndex(23));
+            int y_text = y_item + (h - GetFontYSIZE(font)) / 2;
+            int y2_text = y_text + GetFontYSIZE(font);
+            DrawString(data->ws, x_item, y_text, x2_item, y2_text, font, TEXT_ALIGNMIDDLE,
+                GetPaletteAdrByColorIndex(PC_FOREGROUND), GetPaletteAdrByColorIndex(0x17));
         }
     }
 }
 
-static void OnCreate(MAIN_GUI *data, malloc_func_t) {
-    data->ws = AllocWS(128);
-    data->gui.state = 1;
-}
-
-static void OnClose(MAIN_GUI *data, mfree_func_t) {
-    data->gui.state = 0;
-    FreeWS(data->ws);
-}
-
-static void OnFocus(MAIN_GUI *data, malloc_func_t, mfree_func_t) {
-    DisableIDLETMR();
-    data->gui.state = 2;
-}
-
-static void OnUnFocus(MAIN_GUI *data, malloc_func_t) {
-    if (data->gui.state != 2) {
-        return;
-    }
-    data->gui.state = 1;
-}
-static int OnKey(MAIN_GUI *data, GUI_MSG *msg) {
+static int OnKey(GUI *gui, GUI_MSG *msg) {
+    INSERT_OPERATION_DATA *data = GetPopupUserPointer(gui);
     const int submess = msg->gbsmsg->submess;
     if (msg->gbsmsg->msg == KEY_DOWN) {
         if (((submess >= '0') && (submess <= '9')) || (submess == '*')) {
-            insert_operation(GetOperationIndexByKey(submess) + ((data->state) * 12));
+            insert_operation(GetOperationIndexByKey(submess) + ((data->state + 1) * 12));
             if (SETTINGS.auto_recalc) {
                 data->ui_data->req_recalc = 1;
             }
             return 1;
         }
         else if (submess == '#') {
-            if (++data->state > 3) {
-                return 1;
-            } else {
+            data->state++;
+            if (data->state < 3) {
                 DirectRedrawGUI();
-                return -1;
+            } else {
+                return 1;
             }
         }
         else if (submess == RIGHT_SOFT) {
             return 1;
         }
     }
-    return 0;
+    return -1;
 }
 
-static int method8(void) {
-    return 0;
+static void GHook(GUI *gui, int cmd) {
+    INSERT_OPERATION_DATA *data = GetPopupUserPointer(gui);
+
+    if (cmd == TI_CMD_CREATE) {
+        static GUI_METHODS gui_methods;
+        void **m = GetDataOfItemByID(gui, 30);
+        memcpy(&gui_methods, m[1], sizeof(GUI_METHODS));
+        gui_methods.onRedraw = OnRedraw;
+        m[1] = &gui_methods;
+        data->ws = AllocWS(16);
+    } else if (cmd == TI_CMD_DESTROY) {
+        FreeWS(data->ws);
+    }
 }
 
-static int method9(void) {
-    return 0;
-}
-
-const void * const gui_methods[11]={
-    (void *)OnRedraw,
-    (void *)OnCreate,
-    (void *)OnClose,
-    (void *)OnFocus,
-    (void *)OnUnFocus,
-    (void *)OnKey,
+const POPUP_DESC POPUP_D = {
     0,
-    (void *)kill_data,
-    (void *)method8,
-    (void *)method9,
-    0
+    OnKey,
+    GHook,
+    NULL,
+    NULL,
+    NULL,
+    1,
+    LGP_NULL,
+    NULL,
+    0,
+    FONT_SMALL,
+    101,
+    0,
 };
 
 int InsertOperation_CreateUI(UI_DATA *ui_data) {
-    MAIN_GUI *main_gui = malloc(sizeof(MAIN_GUI));
-    zeromem(main_gui, sizeof(MAIN_GUI));
-    main_gui->ui_data = ui_data;
-
-    CANVAS.x2 = ScreenW() - 1;
-    CANVAS.y2 = ScreenH() - 1;
-
-    main_gui->gui.canvas = &CANVAS;
-    main_gui->gui.flag30 = 2;
-    main_gui->gui.methods = (void *)gui_methods;
-    main_gui->gui.item_ll.data_mfree = (void (*)(void *))mfree_adr();
-    main_gui->state = 1;
-    return CreateGUI(main_gui);
+    INSERT_OPERATION_DATA *data = malloc(sizeof(INSERT_OPERATION_DATA));
+    zeromem(data, sizeof(INSERT_OPERATION_DATA));
+    data->ui_data = ui_data;
+    return CreatePopupGUI(1, data, &POPUP_D, LGP_NULL);
 }
